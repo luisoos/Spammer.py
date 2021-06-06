8abcbbe3f1752cb9486eef24deeadc70e1705e73
Luis <81855420+luisoos@users.noreply.github.com>
2021-06-06 19:36:24 +0200

    Add files via upload

---
+++ spammer.py
@@ -0,0 +1,13 @@
+# This is spammer.py. You can spam the whole text.txt file with it, in just a few lines of python.
+# Fill in the text.txt file with the things that you want to spam, set the time between the
+# individual paragraphs by editing the "time.sleep(YOUR_TIME)" element (I have it set to 3 seconds
+# right now). Then press start and quickly head over to the place, you want to spam in.
+
+print("Starting...")
+import pyautogui, time
+time.sleep(3)
+f = open("text", 'r')
+for word in f:
+        pyautogui.typewrite(word)
+        pyautogui.press("enter")
+
